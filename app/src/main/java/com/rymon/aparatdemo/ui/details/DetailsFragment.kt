4c00d3d9401fff091338ad4bd358dfdc96056f13
Mostafa <mostafa.imani.official@gmail.com>
2022-04-05 15:45:50 +0430

    complete DetailsFragment.kt to show clips

--- app/src/main/java/com/rymon/aparatdemo/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/rymon/aparatdemo/ui/details/DetailsFragment.kt
@@ -2,6 +2,12 @@ package com.rymon.aparatdemo.ui.details
 
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.rymon.aparatdemo.R
 import com.rymon.aparatdemo.databinding.FragmentDetailsBinding
@@ -11,12 +17,19 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
 
 //    private val args by navArgs<>()
 
+    override fun onDetach() {
+        super.onDetach()
+        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         val binding = FragmentDetailsBinding.bind(view)
 
+        binding.apply {
+            loadVideo(webView,"https://www.aparat.com/video/video/embed/videohash/rzKus/vt/frame")
+        }
 /*
         binding.apply {
 //            val photo = args.photo
@@ -65,5 +78,15 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
             }
         }
     */
+    }
+    private fun loadVideo(webView:WebView,vid_url:String) {
+
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.pluginState = WebSettings.PluginState.ON
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl(vid_url)
     }
 }
